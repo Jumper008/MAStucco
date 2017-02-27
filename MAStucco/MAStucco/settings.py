c5c96df9f5135a4814bec1cc0a1d570ace22e560
Alejandro Zamudio Guajardo <alex.zamudio88@gmail.com>
2017-02-27 03:14:59 -0600

    Added bootstrap v.3.3.7 to the web app for design purposes. Further developed base.html and home.html files, adding a default navbar. The CSS of this navbar must be tweaked to achieve desired effect.

--- MAStucco/MAStucco/settings.py
+++ MAStucco/MAStucco/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pn3j2$&8883(5o&@9o+2-=_$5tt==^5_ll8um@9jd)@c=f4)dz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-LOGIN_URL = 'http://127.0.0.1:8080/login/'
+LOGIN_URL = 'http://127.0.0.1:8000/login/'
 
 ALLOWED_HOSTS = []
 
--- MAStucco/WAMAStucco/views.py
+++ MAStucco/WAMAStucco/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib import auth
 
-@login_required()
 def home_view(request):
     return render(request, 'home.html', {'page_title': 'Home'})
 
