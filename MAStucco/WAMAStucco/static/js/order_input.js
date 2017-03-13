9e6c9c4f28c6472326ae99eba6eaf4773fa237b2
Melylennon95 <melylennon@gmail.com>
2017-03-13 00:19:05 -0600

    Avance Order input

--- MAStucco/WAMAStucco/static/js/order_input.js
+++ MAStucco/WAMAStucco/static/js/order_input.js
@@ -5,3 +5,19 @@
 $("#orderInput").click(function() {
    window.location = "http://127.0.0.1:8000/workorders/order_input";
 });
+
+
+$('#add_part').click(function() {
+
+ /*var structure = $('<hr><div id = "part_order"><div class="row"><div class="col-md-2"><label><b>Quantity</b></label></div>' +
+     '<div class="col-md-5"><input class="form-control" type="text" name="Quantity" size="30" placeholder="Quantity...">' +
+    '</div></div><br><div class="row"><div class="col-md-2"><label><b>Part</b></label></div><div class="col-md-5">' +
+    '<input class="form-control" type="text" name="Quantity" size="30" placeholder="Part..."></div></div><br><div class="row"> ' +
+    '<div class="col-md-2"><label><b>Measure</b></label></div><div class="col-md-5">' +
+    '<input class="form-control" type="text" name="Measure" size="30" placeholder="Measure..."> </div></div></div>');*/
+
+ var structure = $('<div class="row"><div class="col-md-5">{{ sub_sub_form.as_p }}</div></div>')
+
+ $('#part_order').append(structure);
+
+})
