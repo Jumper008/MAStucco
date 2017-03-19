6992f4159ca5bdc553f33c3ec66ac0dcc5af9b05
Melylennon95 <melylennon@gmail.com>
2017-03-19 17:09:08 -0600

    'PartOrders'

--- MAStucco/WAMAStucco/forms.py
+++ MAStucco/WAMAStucco/forms.py
@@ -7,10 +7,10 @@ class WorkOrderForm(ModelForm):
         model = WorkOrder
         fields = ['customer', 'order_by', 'model', 'notes']
         widgets = {
-            'customer': TextInput(attrs={'class': 'form-control', 'placeholder': 'Customer...', 'style': 'width:300px;'}),
-            'order_by': TextInput(attrs={'class': 'form-control', 'placeholder': 'Order by...', 'style': 'width:300px;'}),
-            'model': TextInput(attrs={'class': 'form-control', 'placeholder': 'Model...', 'style': 'width:300px;'}),
-            'notes': TextInput(attrs={'class': 'form-control', 'placeholder': 'Notes...', 'style': 'width:300px;'})
+            'customer': TextInput(attrs={'class': 'form-control', 'placeholder': 'Customer...', 'style': 'width:270px;'}),
+            'order_by': TextInput(attrs={'class': 'form-control', 'placeholder': 'Order by...', 'style': 'width:270px;'}),
+            'model': TextInput(attrs={'class': 'form-control', 'placeholder': 'Model...', 'style': 'width:270px;'}),
+            'notes': TextInput(attrs={'class': 'form-control', 'placeholder': 'Notes...', 'style': 'width:270px;'})
         }
 
 
@@ -19,9 +19,9 @@ class JobForm(ModelForm):
         model = Job
         fields = ['lot', 'address', 'subdivision']
         widgets = {
-            'lot': TextInput(attrs={'class': 'form-control', 'placeholder': 'Lot...', 'style': 'width:300px;'}),
-            'address': TextInput(attrs={'class': 'form-control', 'placeholder': 'Address...', 'style': 'width:300px;'}),
-            'subdivision': TextInput(attrs={'class': 'form-control', 'placeholder': 'subdivision...', 'style': 'width:300px;'})
+            'lot': TextInput(attrs={'class': 'form-control', 'placeholder': 'Lot...', 'style': 'width:270px;'}),
+            'address': TextInput(attrs={'class': 'form-control', 'placeholder': 'Address...', 'style': 'width:270px;'}),
+            'subdivision': TextInput(attrs={'class': 'form-control', 'placeholder': 'subdivision...', 'style': 'width:270px;'})
         }
 
 
@@ -30,7 +30,7 @@ class PartOrderForm(ModelForm):
         model = PartOrder
         fields = ['quantity', 'part', 'measure']
         widgets = {
-            'quantity': TextInput(attrs={'class': 'form-control', 'placeholder': 'Quantity...', 'style': 'width:300px;'}),
-            'part': TextInput(attrs={'class': 'form-control', 'placeholder': 'Part...', 'style': 'width:300px;'}),
-            'measure': TextInput(attrs={'class': 'form-control', 'placeholder': 'Measure...', 'style': 'width:300px;'})
+            'quantity': TextInput(attrs={'class': 'form-control', 'placeholder': 'Quantity...', 'style': 'width:270px;'}),
+            'part': TextInput(attrs={'class': 'form-control', 'placeholder': 'Part...', 'style': 'width:270px;'}),
+            'measure': TextInput(attrs={'class': 'form-control', 'placeholder': 'Measure...', 'style': 'width:270px;'})
         }
