9ae8147c4af2c335d280d9b88be9f22bffe6585f
Melylennon95 <melylennon@gmail.com>
2017-03-24 09:02:28 -0600

    'WorkOrders'

--- MAStucco/WAMAStucco/admin.py
+++ MAStucco/WAMAStucco/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from WAMAStucco.models import WorkOrder, Job, PartOrder
+from WAMAStucco.models import WorkOrder, Job, PartOrder, Profile
 
 admin.site.register(WorkOrder)
 admin.site.register(Job)
 admin.site.register(PartOrder)
+admin.site.register(Profile)
