142e48d70b5d47f55643ee9c3edeecedbdaf68b5
Alejandro Zamudio Guajardo <jumper008@Alejandros-MacBook-Pro.local>
2017-05-27 21:49:58 -0500

    Added new global static folder, ran collectstatic and added STATIC_ROOT config in settings.py.

---
+++ MAStucco/static/js/workeradministration.js
@@ -0,0 +1,15 @@
+$("#unactiveW").click(function() {
+   window.location = "http://127.0.0.1:8000/workeradministration_unactive";
+});
+
+$("#activeW").click(function() {
+   window.location = "http://127.0.0.1:8000/workeradministration";
+});
+
+$("#unactiveW2").click(function() {
+   window.location = "http://127.0.0.1:8000/workeradministration_unactive";
+});
+
+$("#activeW2").click(function() {
+   window.location = "http://127.0.0.1:8000/workeradministration";
+});
